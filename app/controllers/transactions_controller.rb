cdb3ba9643f4fac9c3eb17e4e2db9d3386c0308c
Francisco Arevalo <farevalod@gmail.com>
2016-11-02 17:40:33 -0300

    Form and validation

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -3,24 +3,24 @@ class TransactionsController < ApplicationController
 		cuenta = Cuenta.find_by_address(params[:address])
 		if cuenta.nil?
 			flash[:error] = "La cuenta ingresada no existe."
-			redirect_to :root
+			redirect_to :root and return
 		end
-		if cuenta.saldo < params[:amount].to_i
+		if params[:instruction].to_i == 0 and cuenta.saldo < params[:transaction][:amount].to_i
 			flash[:error] = "La cuenta ingresada no tiene saldo suficiente."
-			redirect_to cuenta
+			redirect_to "/cuentas/"+cuenta.address and return
 		end
-		if Digest::SHA256.hexdigest params[:password] != cuenta.address
+		if Digest::SHA256.hexdigest(params[:transaction][:password]) != cuenta.address
 			flash[:error] = "El password ingresado es incorrecto."
-			redirect_to cuenta
+			redirect_to "/cuentas/"+cuenta.address and return
 		end
 		t = Transaction.new
 		t.cuenta_id = cuenta.id
-		t.amount = params[:amount]
-		t.description = params[:description]
-		instruction = params[:instruction]
+		t.amount = params[:transaction][:amount]
+		t.description = params[:transaction][:description]
+		t.instruction = params[:transaction][:instruction].to_i
 		if t.save
 			flash[:success] = "Transaccion creada exitosamente."
 		end
-		redirect_to cuenta
+		redirect_to "/cuentas/"+cuenta.address and return
 	end
 end
