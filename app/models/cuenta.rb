acf9edd9b19bddb7211088af5bba1aebf7aed4bb
Francisco Arevalo <farevalod@gmail.com>
2016-11-02 16:26:31 -0300

    Models y Relationships

--- app/models/cuenta.rb
+++ app/models/cuenta.rb
@@ -1,2 +1,15 @@
 class Cuenta < ActiveRecord::Base
+	has_many :transactions
+	self.table_name = "cuentas"
+	def saldo
+		total = 0
+		transactions.each do |t|
+			if t.deposit?
+				total += t.amount
+			else
+				total -= t.amount
+			end
+		end
+		return total
+	end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,6 @@
 class Transaction < ActiveRecord::Base
+	belongs_to :cuenta
+	def deposit?
+		return instruction == 1
+	end
 end
